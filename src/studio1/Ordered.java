1bff59a3da2301350d6e06a3d53caf32cf8c29f7
bonniechadwick <bonniechadwick@bonnies-air.dhcp.wustl.edu>
2023-01-26 13:39:23 -0600

    Wrote some code.

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -8,6 +8,7 @@ public class Ordered {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		boolean isOrdered;
 		Scanner in = new Scanner(System.in);
 		System.out.println("Value for x?");
 		int x = in.nextInt();
@@ -15,7 +16,8 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		isOrdered = x<y && y<z;
+		System.out.print(isOrdered);
 	}
 
 }
